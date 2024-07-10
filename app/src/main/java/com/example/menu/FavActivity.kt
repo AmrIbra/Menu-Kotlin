1d01cb920e0ddce96cb16bb6d7bb6738a500403a
Amr <amribra12@gmail.com>
2024-07-10 14:34:58 +0300

    Added onCreate function to FavActivity, actual test 1

--- app/src/main/java/com/example/menu/FavActivity.kt
+++ app/src/main/java/com/example/menu/FavActivity.kt
@@ -2,17 +2,22 @@ package com.example.menu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class FavActivity:AppCompatActivity() {
 
     val mealDao=MealDatabase.getInstance(context = this).mealDao()
 
-    val fav_data=mealDao.getAll()
+    val fav_data=mealDao.getAll().toList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fav)
+        val recyclerView: RecyclerView =findViewById(R.id.recyclerView2)
+        recyclerView.adapter=FavAdapter(fav_data, this)
+        recyclerView.layoutManager= LinearLayoutManager(this)
 
     }
 }
