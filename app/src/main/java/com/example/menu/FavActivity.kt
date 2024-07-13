920f51db1c5f12c67b8c8e7f46983858d57e6ee0
Amr <amribra12@gmail.com>
2024-07-13 11:46:25 +0300

    Solved Coroutine Problem, added lifecycleScope

--- app/src/main/java/com/example/menu/FavActivity.kt
+++ app/src/main/java/com/example/menu/FavActivity.kt
@@ -1,23 +1,37 @@
 package com.example.menu
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.withContext
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
 
 class FavActivity:AppCompatActivity() {
 
-    val mealDao=MealDatabase.getInstance(context = this).mealDao()
 
-    val fav_data=mealDao.getAll().toList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fav)
-        val recyclerView: RecyclerView =findViewById(R.id.recyclerView2)
-        recyclerView.adapter=FavAdapter(fav_data, this)
-        recyclerView.layoutManager= LinearLayoutManager(this)
+        lifecycleScope.async {
+            val mealDao=MealDatabase.getInstance(context =this@FavActivity).mealDao()
+
+            withContext(Dispatchers.IO){
+                val fav_data=mealDao.getAll().toList()
+                val recyclerView: RecyclerView =findViewById(R.id.recyclerView2)
+                recyclerView.adapter=FavAdapter(fav_data, this@FavActivity)
+                recyclerView.layoutManager= LinearLayoutManager(this@FavActivity)
+                //withContext(Dispatchers.Main){}
+            }
+
+        }
+
 
     }
 }
